e7b5ff6bbc7a8423c2221491d08e60faf4eb72c5
Vince Cooley <vince.r.cooley@gmail.com>
2016-11-25 10:09:50 -0600

    deployment

--- migrations/20160726211034_test.js
+++ migrations/20160726211034_test.js
@@ -1,28 +1,25 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('books', function(table) {
+  return Promise.all([
+    knex.schema.createTable('books', function(table) {
       table.increments('id').primary();
       table.string('title');
-      return table.string('description');
+      table.string('description');
+    }),
+    knex.schema.createTable('users_books', function(table) {
+      table.integer('user_id').references('users.id');
+      table.integer('book_id').references('books.id');
+    }),
+    knex.schema.createTable('trades', function(table) {
+      table.increments('id').primary();
+      table.integer('requester').references('users.id');
+      table.integer('requestee').references('users.id');
+      table.integer('requester_book').references('books.id');
+      table.integer('requestee_book').references('books.id');
+      table.boolean('requester_approval').defaultTo(false);
+      table.boolean('requestee_approval').defaultTo(false);
     })
-    .then(function() {
-      return knex.schema.createTable('users_books', function(table) {
-        table.integer('user_id').references('users.id');
-        return table.integer('book_id').references('books.id');
-      })
-    })
-    .then(function() {
-      return knex.schema.createTable('trades', function(table) {
-        table.increments('id').primary();
-        table.integer('requester').references('users.id');
-        table.integer('requestee').references('users.id');
-        table.integer('requester_book').references('books.id');
-        table.integer('requestee_book').references('books.id');
-        table.boolean('requester_approval').defaultTo(false);
-        return table.boolean('requestee_approval').defaultTo(false);
-      });
-    });
+  ]);
 };
-
 
 exports.down = function(knex, Promise) {
   return Promise.all([
